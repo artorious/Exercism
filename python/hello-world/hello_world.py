c8dbcca11b02d46c6d04870e71b5d5c3fc0798eb
Arthur Ngondo <ngondoarthur@gmail.com>
2018-06-09 19:42:28 +0300

    Add failing tests for hello world exercise

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -1,2 +1,13 @@
+""" Hello World """
 def hello(name=''):
+    """ (str) -> str
+
+        Returns "Hello <name>!" when param is provided.
+        Else, "Hello World!"
+    """
+    #try:
+    #    if isinstance(name, str):
+    #        pass
+    #except:
+    #    raise TypeError('Argument should be a string')
     pass
--- python/hello-world/hello_world_test.py
+++ python/hello-world/hello_world_test.py
@@ -7,8 +7,16 @@ import hello_world
 
 class HelloWorldTests(unittest.TestCase):
     def test_hello(self):
+        """ Tests function returns when called without a parameter."""
         self.assertEqual(hello_world.hello(), 'Hello, World!')
 
+    
+    def test_str_input(self):
+        """ Tests function returns when called with string as parameter."""
+        self.assertEqual(hello_world('Arthur'), 'Hello, Arthur!')
 
+    def test_it_only_accepts_strings(self):
+        """ Tests function raises TypeError if called with a non-string """
+        self.assertRaises(hello_world.TypeError, hello_world.hello, 1)
 if __name__ == '__main__':
     unittest.main()
